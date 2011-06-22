58ddca5639bda2b19066fdd889c70fe59af626c9
Steinthor Jasonarson <steinthor@gmail.com>
2011-06-22 22:29:14 +0000

    fixed issue 15, problem with opening revisions of tiddlers

--- handle/revisionDisplay.php
+++ handle/revisionDisplay.php
@@ -27,8 +27,32 @@ $tiddler_list = db_tiddlers_backupSelectOid($tiddler['id']);
 //find revision
 foreach( $tiddler_list as $t ) {
 	if( $revision == $t['revision'] ) {		//if revision equals, check privilege
-		if( user_readPrivilege(user_tiddlerPrivilegeOfUser($user,$t['tags'])) ) {	//if read privilege ok, output		
-			echo '{"created":"'.$t['created'].'", "text":"'.tiddler_bodyEncode($t['body']).'", "tags":"'.$t['tags'].'", "modified":"'.$t['modified'].'", "modifier":"'.$t['modifier'].'", "revision":'.$t['revision'].'}';
+		if( user_readPrivilege(user_tiddlerPrivilegeOfUser($user,$t['tags'])) ) {	//if read privilege ok, output	
+			//echo '{"created":"'.$t['created'].'", "text":"'.tiddler_bodyEncode($t['body']).'", "tags":"'.$t['tags'].'", "modified":"'.$t['modified'].'", "modifier":"'.$t['modifier'].'", "revision":'.$t['revision'].'}';
+
+			$result = '{"created":"';
+			if(isset($t['created']))
+				$result .= $t['created'];
+			else $result .= $t['modified'];
+			$result .= '", "text":"';
+			if(isset($t['body']))
+				$result .= tiddler_bodyEncode($t['body']);
+			$result .= '", "tags":"';
+			if(isset($t['tags']))
+				$result .= $t['tags'];
+			$result .= '", "modified":"';
+			if(isset($t['modified']))
+				$result .= $t['modified'];
+			$result .= '", "modifier":"';
+			if(isset($t['modifier']))
+				$result .= $t['modifier'];
+			$result .= '", "revision":';
+			if(isset($t['revision']))
+				$result .= $t['revision'];
+			$result .= '}';
+			
+			echo $result;
+			
 		}else{		//if no read privilege, stop
 			sendHeader(204);
 		}
